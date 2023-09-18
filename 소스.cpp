616fc5ebb30342333c6b77ed7f0cdad7e6b81a57
SuHyeon <dltngus12323@gmail.com>
2023-09-18 17:21:03 +0900

    랜덤 엔진과 분포

--- 소스.cpp
+++ 소스.cpp
@@ -1,29 +1,42 @@
-// 2023. 9. 11 월										(2주 1일)
+// 2023. 9. 13 수										(2주 2일)
+// 다음 주 수요일 9/20 - 전시로 수업(3주 2일)을 대체 함
 //---------------------------------------------------------------------------
-// C++ 입출력 - C++20기준
-//---------------------------------------------------------------------------
-// 과제 C++ I/O 관련 내용 읽어보기
+// 랜덤 엔진과 분포
 //---------------------------------------------------------------------------
 
 #include <iostream>
+#include <random>			// 랜덤값과 분포를 사용하려면
+#include <format>
+#include <algorithm>
 #include "save.h"
 using namespace std;
 
+default_random_engine dre;				// 랜덤값을 생성하는 엔진
+uniform_int_distribution uid{ 1, 999 };
+
 //--------
-int main()				// cin, cout, cerr, clog
+int main()
 //--------
 {
-	// [문제] int 값 2를 입력받아 합계를 출력하라
-	int a, b;
+	int numbers[100];
 
-	cout << "두 수의 합 구하기" << endl;
-	cout << "a를 입력하세요" << endl;
-	cin >> a;
-	cout << "b를 입력하세요" << endl;
-	cin >> b;
+	for (int i = 0; i < 100; ++i) {
+		numbers[i] = uid(dre);
+	}
 
-	int result = a + b;
-	cout << "합계: " << result << endl;
+	for (int num : numbers) {
+		cout << format("{:4d}", num);
+	}
+	cout << endl;
+
+	int max = numeric_limits<int>::min();
+	// [문제] 제일 큰 값을 찾아 화면에 입력하시오
+	for (int num : numbers) {
+		if (num > max) {
+			max = num;
+		}
+	}
+	cout << "제일 큰 값 - " << max << endl;
 
 	save("소스.cpp");
 }

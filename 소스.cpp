73399120f5ac72a598aae67232e861ea87d43915
SuHyeon <dltngus12323@gmail.com>
2023-09-18 18:01:06 +0900

    [문제] 원하는 수를 하나 입력받아라 그 수가 numbers에 있는 지 없는 지 찾아 출력하라

--- 소스.cpp
+++ 소스.cpp
@@ -1,7 +1,7 @@
-// 2023. 9. 13 수										(2주 2일)
-// 다음 주 수요일 9/20 - 전시로 수업(3주 2일)을 대체 함
+// 2023. 9. 18 월										(3주 1일)
+// 이번 주 수요일 9/20 - 전시로 수업(3주 2일)을 대체 함
 //---------------------------------------------------------------------------
-// 랜덤 엔진과 분포
+// 많은 수의 int가 있다 - 어떻게 저장할건가? 어떤 작업을 하고 싶나?
 //---------------------------------------------------------------------------
 
 #include <iostream>
@@ -18,9 +18,9 @@ uniform_int_distribution uid{ 1, 999 };
 int main()
 //--------
 {
-	int numbers[100];
+	int numbers[1000];
 
-	for (int i = 0; i < 100; ++i) {
+	for (int i = 0; i < 1000; ++i) {
 		numbers[i] = uid(dre);
 	}
 
@@ -30,13 +30,27 @@ int main()
 	cout << endl;
 
 	int max = numeric_limits<int>::min();
-	// [문제] 제일 큰 값을 찾아 화면에 입력하시오
-	for (int num : numbers) {
-		if (num > max) {
-			max = num;
+	// [문제] 원하는 수를 하나 입력받아라
+	// 그 수가 numbers에 있는 지 없는 지 찾아 출력하라
+
+	int x;
+	while(true) {
+		cout << "원하는 수를 입력하시오" << endl;
+		cin >> x;
+
+		bool flag{ false };
+		for (int num : numbers) {
+			if (num == x) {
+				flag = true;
+				cout << "해당 수를 찾았습니다" << endl;
+				break;
+			}
+		}
+
+		if (not flag) {
+			cout << "해당 수를 못 찾았습니다" << endl;
 		}
 	}
-	cout << "제일 큰 값 - " << max << endl;
-
-	save("소스.cpp");
+	
+	//save("소스.cpp");
 }

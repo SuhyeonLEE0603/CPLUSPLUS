ee1451a1fb0d186e6dc7c126c0050261181050e3
Lee Su Hyeon <dltngus12323@gmail.com>
2023-09-27 17:01:29 +0900

    [문제] num에 있는 값 중 제일 큰 값을 오른쪽으로 옮겨라

--- 소스.cpp
+++ 소스.cpp
@@ -11,17 +11,28 @@
 #include "save.h"
 using namespace std;
 
-void change(int&, int&);			// call-by reference
+void change(int&, int&);			// 선언과 정의를 반드시 나눠야함
 
 //--------
 int main()
 //--------
 {
-	int a{ 1 }, b{ 2 };
+	int num[]{ 10, 3, 8, 6, 1, 5, 2, 7, 4, 9 };
 
-	change(a, b);
+	// [문제] num에 있는 값 중 제일 큰 값을 오른쪽으로 옮겨라
+	int size = sizeof(num) / sizeof(int);
 
-	cout << a << ", " << b << endl;
+	for (int i = 0; i < size - 1; ++i) {
+		if (num[i] > num[i + 1]) {
+			change(num[i], num[i + 1]);
+		}
+	}
+
+	for (int num : num) {
+		cout << num << " ";
+	}
+	cout << endl;
+
 	save("소스.cpp");
 }
 

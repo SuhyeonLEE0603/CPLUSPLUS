1b57e4ad008301b2ca5be9450a5bb70a54aa0f02
Lee Su Hyeon <dltngus12323@gmail.com>
2023-10-04 16:12:59 +0900

    qsort 비교함수 다른 방법으로 코딩

--- 소스.cpp
+++ 소스.cpp
@@ -1,4 +1,5 @@
-// 2023. 10. 2 월										(5주 1일)
+// 2023. 10. 4 수										(5주 2일)
+// 10월 9일(6주 1일) - 동영상 강의
 // 10월 25일(8주 2일) - 중간시험
 //---------------------------------------------------------------------------
 // 정렬(sort) - C언어 라이브러리 함수 qsort
@@ -21,9 +22,9 @@ uniform_int_distribution uid{ 1, 99'9999 };
 int main()
 //--------
 {
-	const int NUM{ 1000 };
+	const int NUM{ 20'0000 };
 	int num[NUM];
-
+	
 	// &는 원래 데이터에 붙인 별명이니까 원본을 그대로 사용하고 싶을 떄 쓴다
 	for (int& number : num) {
 		number = uid(dre);
@@ -31,8 +32,8 @@ int main()
 
 	// 정렬 - qsort
 
-	int 정렬방법(const void*,const void*);
-	qsort(num, NUM, sizeof(int), 정렬방법);
+	int 비교함수(const void*,const void*);
+	qsort(num, NUM, sizeof(int), 비교함수);		// generic function
 
 	// 정렬 후 출력
 	for (int num : num) {
@@ -42,18 +43,7 @@ int main()
 	save("소스.cpp");
 }
 
-int 정렬방법(const void* p, const void* q) {
-	int a = *(int*)p;
-	int b = *(int*)q;
-
-	// 오름차순
-	if (a > b) {
-		return 1;
-	}
-	else if (a < b) {
-		return -1;
-	}
-
-	return 0;
+int 비교함수(const void* p, const void* q) {
+	return *(int*)p - *(int*)q;
 
 }

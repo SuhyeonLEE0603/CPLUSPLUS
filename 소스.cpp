0c0305324c56cd7259ffcc9e71ecab53fd9859b4
Lee Su Hyeon <dltngus12323@gmail.com>
2023-10-04 14:04:25 +0900

    내림차순 정렬 및 NUM(배열 크기)추가

--- 소스.cpp
+++ 소스.cpp
@@ -15,23 +15,24 @@ uniform_int_distribution uid{ 1, 999 };
 
 // [문제] int 값 1'000개를 저장할 수 있는 배열을 만들어라
 // 각 int값을 1부터 999까지 값 중 랜덤값으로 만들어라
-// 배열을 오름차순으로 정렬하라
+// 배열을 내림차순(descending order)으로 정렬하라
 // 전체 1000개 값을 화면에 출력하라
 
 //--------
 int main()
 //--------
 {
-	int num[1000];		// 4KB
+	const int NUM{ 1000 };
+	int num[NUM];		
 
-	for (int i = 0; i < 1000; ++i) {
+	for (int i = 0; i < NUM; ++i) {
 		num[i] = uid(dre);
 	}
 
-	// 정렬 - qsort
-	for (int j = 0; j < 1000 - 1; ++j) {
-		for (int i = 0; i < 1000 - 1; ++i) {
-			if (num[i] > num[i + 1]) {
+	// 정렬 - bubble sort
+	for (int j = 0; j < NUM - 1; ++j) {
+		for (int i = 0; i < NUM - 1; ++i) {
+			if (num[i] < num[i + 1]) {
 				int temp{ num[i] };
 				num[i] = num[i + 1];
 				num[i + 1] = temp;

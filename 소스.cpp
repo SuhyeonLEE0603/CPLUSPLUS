4ddfc3f483d16a94e0f9e180ee1e62c95b1f1a4b
Lee Su Hyeon <dltngus12323@gmail.com>
2023-10-18 16:37:57 +0900

    [실습] 랜덤값을 갖는 Point2D 100개 만들어라 (x, y)값은 [-100, 100] 이다 원점(0, 0)에서 반경 10미만까지 영향을 미치는 폭탄이 터졌다 어떤 점이 이 폭탄에 영향을 받는 지 출력하라 root 값을 구할때는 sqrt 함수를 사용하면 된다

--- 소스.cpp
+++ 소스.cpp
@@ -7,27 +7,44 @@
 //---------------------------------------------------------------------------
 
 #include <iostream>
+#include <random>
 #include "save.h"
 using namespace std;
 
-struct Test{
-public:			// access-modifier, public, private, protected
-	int n;
-	char c;
+default_random_engine dre;
+uniform_int_distribution uid{ -100, 100 };
+
+struct Point2D{
+	// data member, member variable
+	int x;
+	int y;
+
+	void show() {
+		cout << "(" << x << ", " << y << ")" << endl;
+	}
 };
+
+// [실습] 랜덤값을 갖는 Point2D 100개 만들어라
+// (x, y)값은 [-100, 100] 이다
+// 원점(0, 0)에서 반경 10미만까지 영향을 미치는 폭탄이 터졌다
+// 어떤 점이 이 폭탄에 영향을 받는 지 출력하라
+// root 값을 구할때는 sqrt 함수를 사용하면 된다
 
 //--------
 int main()
 //--------
 {
-	Test t;
+	Point2D points[100];	// Point2D : 사용자 정의 자료형, p : 객체
+	
+	for (Point2D& point : points) {
+		point.x = uid(dre);
+		point.y = uid(dre);
+	}
 
-	t.c = 'a';
-	t.n = 123;
-
-	cout << sizeof(t) << endl;
-	// 8바이트가 잡히는 이유는 컴퓨터 자체에서 5바이트씩 읽는 것 보다 메모리를 더 소모하여
-	// 4바이트 단위로 읽는 것이 훨씬 빠르기 때문(Padding)
-
+	for (Point2D& point : points) {
+		if (sqrt(point.x * point.x + point.y * point.y) < 10) {
+			point.show();
+		}
+	}
 	save("소스.cpp");
 }

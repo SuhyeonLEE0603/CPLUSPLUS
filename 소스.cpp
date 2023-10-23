9813d7d81d99983f25bc86d43131b37a19aae1e5
Lee Su Hyeon <dltngus12323@gmail.com>
2023-10-23 17:45:04 +0900

    class dog 생성(생성자 설명)

--- 소스.cpp
+++ 소스.cpp
@@ -1,50 +1,37 @@
-// 2023. 10. 18 수										(7주 2일)
-// 10월 25일(8주 2일) - 중간시험
+// 2023. 10. 23 월										(8주 1일)
+// 10월 25일(8주 2일) - 중간시험, class 문제 x
 //---------------------------------------------------------------------------
-// 사용자 정의 자료형
+// class
 //---------------------------------------------------------------------------
 // [과제] 나만의 자료형을 많이 만들고 반복 학습
 //---------------------------------------------------------------------------
 
 #include <iostream>
-#include <random>
+#include <Windows.h>
+#include <mmsystem.h>
+#include <string>
 #include "save.h"
+
+#pragma comment (lib, "winmm.lib")
+
 using namespace std;
 
-default_random_engine dre;
-uniform_int_distribution uid{ -100, 100 };
-
-struct Point2D{
-	// data member, member variable
-	int x;
-	int y;
-
-	void show() {
-		cout << "(" << x << ", " << y << ")" << endl;
+class Dog {
+	string name;
+	int age;
+	 
+public:
+	Dog() {
+		cout << "생성자가 호출됨" << '\n';
+		PlaySound(L"dog-bark6.wav", NULL, SND_FILENAME | SND_ASYNC);
 	}
 };
-
-// [실습] 랜덤값을 갖는 Point2D 100개 만들어라
-// (x, y)값은 [-100, 100] 이다
-// 원점(0, 0)에서 반경 10미만까지 영향을 미치는 폭탄이 터졌다
-// 어떤 점이 이 폭탄에 영향을 받는 지 출력하라
-// root 값을 구할때는 sqrt 함수를 사용하면 된다
 
 //--------
 int main()
 //--------
 {
-	Point2D points[100];	// Point2D : 사용자 정의 자료형, p : 객체
+	Dog dog[5];
 	
-	for (Point2D& point : points) {
-		point.x = uid(dre);
-		point.y = uid(dre);
-	}
-
-	for (Point2D& point : points) {
-		if (sqrt(point.x * point.x + point.y * point.y) < 10) {
-			point.show();
-		}
-	}
 	save("소스.cpp");
 }

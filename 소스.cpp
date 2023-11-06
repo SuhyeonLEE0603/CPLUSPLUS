4a8f68780ed3a3d25f415c23cdfd728ae95fc583
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-06 18:20:06 +0900

    class STRING 시작

--- 소스.cpp
+++ 소스.cpp
@@ -6,65 +6,43 @@
 //---------------------------------------------------------------------------
 
 #include <iostream>
+#include <string>
 #include "save.h"
 
 using namespace std;
-
-// [문제] 나는 Memory Monster다. 생성시에 int 값 num을 전달하면
-// Free store에 num개의 int를 저장할 공간을 확보하고 그 값을 모두 num으로 채운다.
-// 다음 코드가 문제없이 실행되도록 코딩하자
-
-class MemoryMonster {
-	int num;
-	int* p;
+class STRING {
+	size_t size;
+	char* p;
 public:
-	
-	MemoryMonster(int n) : num{ n } {
-		p = new int[num];
-		for (int i = 0; i < num; ++i) {
-			p[i] = num;
-		}
-		cout << "MemoryMonster 생성 - " << num << endl;
-	}
 
-	~MemoryMonster() {
-		cout << "MemoryMonster 소멸 - " << num << endl;
+	STRING(const char* s) : size{strlen(s)} {
+		p = new char[size];
+		memcpy(p, s, size);
+
+		cout << "STRING(const char*) - " << size << ", 번지:" << (void*)p << endl;
+	}
+	
+	~STRING() {
+		cout << "~STRING() - " << size << ", 번지:" << (void*)p << endl;
 		delete[] p;
 	}
 
-	MemoryMonster(const MemoryMonster& other) : num{ other.num } {
-		p = new int[num];
-		memcpy(p, other.p, sizeof(int) * num);
-
-		cout << "메몬을 복사함 - " << num << endl;
-	}
-
-	// 복사생성자를 코딩했다면 반드시 이와 짝이 되는 복사할당연산자(copy assignment)를 코딩해야만 한다
-	MemoryMonster& operator=(const MemoryMonster& other) {
-		delete[] p;		// 내가 관리하는 자원을 해제
-		num = other.num;
-		p = new int[num];
-		memcpy(p, other.p, sizeof(int) * num);
-
-		cout << "메몬을 복사할당함 - " << num << endl;
-
-		return *this;
-	}
 	void show() {
-		cout << "Monster - " << num << endl;
+		for (int i = 0; i < size; ++i) {
+			cout << p[i];
+		}
+
+		cout << endl;
 	}
-
-
 };
 
 //--------
 int main()
 //--------
-{
-	MemoryMonster a{ 100 };
-	MemoryMonster b{ 500 };
-
-	b = a;			// copy assignment -> operator=()
-
-	save("소스.cpp");
+{	
+	// STRING의 생성자는 const char* 로 전달하고 처리하자. string 사용하지 말 것
+	STRING s{ "안녕? 난 C의 char*를 완벽하게 대체하는 객체야!" };
+	
+	s.show();
+	//save("소스.cpp");
 }

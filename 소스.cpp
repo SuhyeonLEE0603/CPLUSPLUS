eac4e6b7259fe15464db5e1eb1de777c241fef62
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-06 18:43:11 +0900

    STRING 복사할당연산자

--- 소스.cpp
+++ 소스.cpp
@@ -35,6 +35,20 @@ public:
 		cout << "STRING 복사생성 - " << size << ", 번지:" << (void*)p << endl;
 	}
 
+	STRING& operator=(const STRING& other) {
+		if (this == &other) {
+			return *this;
+		}
+
+		delete[] p;
+		size = other.size;
+		p = new char[size];
+		memcpy(p, other.p, size);
+
+		cout << "STRING 복사할당연산자 - " << size << ", 번지:" << (void*)p << endl;
+		return *this;
+	}
+
 	void show() {
 		for (int i = 0; i < size; ++i) {
 			cout << p[i];
@@ -50,9 +64,13 @@ int main()
 {	
 	// STRING의 생성자는 const char* 로 전달하고 처리하자. string 사용하지 말 것
 	STRING s{ "난 STRING이야!" };
-	STRING t = s;
+	STRING t{ "오늘은 비가 왔다" };
 
-	t.show();
 	s.show();
-	save("소스.cpp");
+	t.show();
+
+	t = s;
+	t.show();
+
+	//save("소스.cpp");
 }

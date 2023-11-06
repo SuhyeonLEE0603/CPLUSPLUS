fc4e2777f759a8652ee79d9ba13abc34e7849064
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-06 17:43:35 +0900

    복사할당연산자

--- 소스.cpp
+++ 소스.cpp
@@ -1,3 +1,10 @@
+// 2023. 11. 6 월										(10주 1일)
+//---------------------------------------------------------------------------
+// class
+//---------------------------------------------------------------------------
+// [과제] 나만의 자료형을 많이 만들고 반복 학습
+//---------------------------------------------------------------------------
+
 #include <iostream>
 #include "save.h"
 
@@ -11,32 +18,53 @@ class MemoryMonster {
 	int num;
 	int* p;
 public:
-
+	
 	MemoryMonster(int n) : num{ n } {
 		p = new int[num];
 		for (int i = 0; i < num; ++i) {
 			p[i] = num;
 		}
+		cout << "MemoryMonster 생성 - " << num << endl;
 	}
 
+	~MemoryMonster() {
+		cout << "MemoryMonster 소멸 - " << num << endl;
+		delete[] p;
+	}
+
+	MemoryMonster(const MemoryMonster& other) : num{ other.num } {
+		p = new int[num];
+		memcpy(p, other.p, sizeof(int) * num);
+
+		cout << "메몬을 복사함 - " << num << endl;
+	}
+
+	// 복사생성자를 코딩했다면 반드시 이와 짝이 되는 복사할당연산자(copy assignment)를 코딩해야만 한다
+	MemoryMonster& operator=(const MemoryMonster& other) {
+		delete[] p;		// 내가 관리하는 자원을 해제
+		num = other.num;
+		p = new int[num];
+		memcpy(p, other.p, sizeof(int) * num);
+
+		cout << "메몬을 복사할당함 - " << num << endl;
+
+		return *this;
+	}
 	void show() {
 		cout << "Monster - " << num << endl;
 	}
 
-	~MemoryMonster() {
-		delete[] p;
-	}
+
 };
 
 //--------
 int main()
 //--------
 {
-	MemoryMonster a{ 123 };
-	MemoryMonster b{ 3333 };
+	MemoryMonster a{ 100 };
+	MemoryMonster b{ 500 };
 
-	a.show();		// 화면에 "Monster - 123" 이라고 출력한다
-	b.show();
+	b = a;			// copy assignment -> operator=()
 
 	save("소스.cpp");
 }

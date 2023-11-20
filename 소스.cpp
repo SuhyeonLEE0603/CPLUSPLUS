9daf785240296aa2fc1bd0d25d9864b3c66041d7
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-20 18:38:12 +0900

    OOP 설명

--- 소스.cpp
+++ 소스.cpp
@@ -5,42 +5,18 @@
 //---------------------------------------------------------------------------
 
 #include <iostream>
-#include <algorithm>
 #include "save.h"
 #include "STRING.h"
 using namespace std;
 
-// 호출할 수(callable) 있는 것은 함수뿐이 아니다
-// 1. 함수
-// 2. 함수를 가리키는 포인터를 호출할 수 있다
-// 3. () 연산자를 오버로딩한 객체를 호출할 수 있다 - function object
-
-// Callable Type - 호출가능한 타입
-
-// Dog가 ()연산자를 오버로딩 - () - function call operator
+// OOP
+// 1. Information Hiding - private
+// 2. Inheritance - code reuse
+// 3. Polymorphism - 다형성
 
 //--------
 int main()
 //--------
 {
-	int a[]{ 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
-	
-	class Dog {
-	public:
-		int operator()(int a, int b) {
-			return a < b;
-		}
-	};
-
-	Dog dog;
-
-	// C++에서 정렬
-	sort(begin(a), end(a), dog);
-
-	for (int i : a) {
-		cout << i << " ";
-	}
-	cout << endl;
-
 	save("소스.cpp");
 }

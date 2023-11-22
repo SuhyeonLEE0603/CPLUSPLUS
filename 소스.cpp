3c82ce95588a994463d44c8eb0e0bed2494b3ec7
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-22 16:07:43 +0900

    class 마무리 설명

--- 소스.cpp
+++ 소스.cpp
@@ -4,74 +4,34 @@
 // 2023. 12. 11 월(15주 1일) - 한학기 정리, 책 소개
 // 2023. 12. 13 수(15주 2일) - 기말시험
 //---------------------------------------------------------------------------
-
 #include <iostream>
-#include <random>
-#include <fstream>
 #include "save.h"
 #include "STRING.h"
 using namespace std;
 
-// [문제] "개20마리.txt" 에는 class Dog 객체 20개의 정보가 기록되어 있다
-// class Dog{
-//		int age;
-//		string name;
-// 맴버 변수가 있으며
-//
-//		friend ostream& operator<<(ostream& os, const Dog& dog){
-//		return os << dog.age << "\t" << dog.name << "\t";
-//		}
-// };
-// 연산자 << 를 오버로딩하여 파일에 기록하였다
-//
-// 20마리 정보를 읽어 나이오름차순으로 정렬하여 출력하라
-
-class Dog {
-	int age;
-	string name;
-
+class X {
+private:
+	// data member;
 public:
+	// function member;
+	// 1. special member;
+	// 2. opreator overloading;
+	// 3. getter/setter interface;
+	// 4. friend (>>, <<)
 
-	// special 함수를 코딩할 이유가 전혀 없음
-
-	int getAge() const {
-		return age;
-	}
-
-	friend ostream& operator<<(ostream& os, const Dog& dog) {
-		return os << dog.age << "\t" << dog.name << "\t";
-	}
-	friend istream& operator>>(istream& is, Dog& dog) {
-		return is >> dog.age >> dog.name;
-	}
-
+	// 자원을 관리(RAII)
+	// 소멸자에서 해제를 해줘야함
+	// smart*를 사용했으면 special function을 반드시 코딩해야함
 };
 
 //--------
 int main()
 //--------
 {
-	ifstream in{ "개20마리.txt" };
-	if (!in) {
-		cout << "파일을 읽지 못했다" << endl;
-		return 0;
-	}
 
-	Dog dogs[20];
+	// X a;			// a를 instancing, Data(전역에 정의시)
+	// {X  a;}		// Stack
+	// X* a = new X;	// Free Store
 	
-	for (int i = 0; i < 20; ++i) {
-		in >> dogs[i];
-	}
-
-	qsort(dogs, sizeof(dogs) / sizeof(Dog), sizeof(Dog), [](const void* a, const void* b) {
-		return ((Dog*)a)->getAge() - ((Dog*)b)->getAge();
-		});
-
-	cout << "나이 (ascending order)순으로 정렬한 결과" << endl;
-
-	for (const Dog& dog : dogs) {		// const & 꼭 붙일것
-		cout << dog << endl;
-	}
-
 	save("소스.cpp");
 }

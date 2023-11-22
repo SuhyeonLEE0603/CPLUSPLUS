0d05f2f93d1d9da621b7bd592e4c9749f1df0329
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-22 17:19:46 +0900

    상속 설명

--- 소스.cpp
+++ 소스.cpp
@@ -9,29 +9,63 @@
 #include "STRING.h"
 using namespace std;
 
-class X {
-private:
-	// data member;
-public:
-	// function member;
-	// 1. special member;
-	// 2. opreator overloading;
-	// 3. getter/setter interface;
-	// 4. friend (>>, <<)
+// 상속(Inheritance)
+// 1. 코드 재사용
+// 2. 다형성 구현 - One interface, multiple method(behavior)
 
-	// 자원을 관리(RAII)
-	// 소멸자에서 해제를 해줘야함
-	// smart*를 사용했으면 special function을 반드시 코딩해야함
+// 상속 연습 동물 개 새를 상속계층(hierarchy)으로 만들어 본다
+
+class Animal {
+	int age{ 0 };
+
+public:
+	Animal() {
+		cout << "Animal의 기본생성자" << endl;
+	}
+
+	Animal(int n) : age{ n } {
+
+	}
+
+	~Animal() {
+		cout << "Animal의 소멸자" << endl;
+	}
+	void move() const {
+		cout << "동물 움직인다 - " << age << endl;
+	}
+
+};
+
+class Dog : public Animal {
+	bool bite{ false };
+
+public:
+	Dog() {
+		cout << "Dog의 기본생성자" << endl;
+	}
+
+	Dog(int n) : Animal(n) {
+
+	}
+
+	~Dog() {
+		cout << "Dog의 소멸자" << endl;
+	}
 };
 
 //--------
 int main()
 //--------
 {
-
-	// X a;			// a를 instancing, Data(전역에 정의시)
-	// {X  a;}		// Stack
-	// X* a = new X;	// Free Store
+	Dog dog{ 10 };
 	
+	// [문제 1] dog의 메모리를 관찰하자
+	cout << "Dog의 메모리 크기 : " << sizeof(dog) << endl;
+
+	// [문제 2] 생성자와 소멸자의 호출을 관찰하자
+	dog.move();
+
+	// [문제 3] Dog의 나이를 설정하자
+
 	save("소스.cpp");
 }

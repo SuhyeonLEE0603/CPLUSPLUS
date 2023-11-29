b1516a52497876b6124f9d16fbc94e0b4b691a98
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-29 16:37:42 +0900

    abstract class, pure virtual, dynamic_cast

--- 소스.cpp
+++ 소스.cpp
@@ -16,11 +16,15 @@ using namespace std;
 // 전부 virtual, override을 명시적으로 붙여줘서 가독성을 높임
 // final을 붙이면 더 이상 자식 클래스를 만들어도 override 금지
 
+
+// Animal은 모든 동물의 공통점을 표현한 abstract 클래스가 되는 것이 바람직함
+// 어떤 클래스가 한 개 이상의 순수가상함수(pure virtual function)가 있다면
+// 그 클래스는 추상 클래스가 되고 객체를 만들 수 없게 됨
+// Pure virtual function은 함수의 몸체가 없는 함수임
 class Animal {
+	int a;
 public:
-	virtual void move() const {
-		cout << "동물 - 움직인다" << endl;
-	}
+	virtual void move() const = 0;		// move는 function body가 없는 순수가상함수 -> (함수 = 0;)으로 표현함
 };
 
 class Dog : public Animal {
@@ -36,9 +40,15 @@ public:
 		cout << "새 - 난다" << endl;
 	}
 };
+// [문제] 동물을 관리하는 코드를 만든다
+// 몇 마리를 관리할 지는 사용자 입력에 따른다
+// 랜덤값에 따라 홀수면 Dog, 짝수면 Bird를 생성하라
+// 모든 동물의 move()를 호출하여 다형성이 구현됨을 확인하라
+// 이 과정이 영원히 반복시행 되도록 관리하라
+// Bird인 경우에만 move()를 호출하고 싶다
 
-// 다음시간 - 동적관리시스템
-// 
+// 다음시간 - 동적관리예제 abstract, pure virtual
+// dyanmic_cast 까지
 
 random_device rd;
 default_random_engine dre{ rd() };
@@ -67,8 +77,13 @@ int main()
 			}
 		}
 
+		// 원래 Bird인 경우에만 move()
+		// RTTI(Real_Time Type Identification)
+
 		for (int i = 0; i < num; ++i) {
-			animals[i]->move();
+			if (dynamic_cast<Bird*>(animals[i]) != nullptr) {
+				animals[i]->move();
+			}
 		}
 
 		for (int i = 0; i < num; ++i) {

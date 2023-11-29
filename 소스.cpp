0acfc788ba5aac984b348b4e8275beb44456487f
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-29 15:40:50 +0900

    [문제] 동물 10마리를 관리하는 코드를 만든다 랜덤값에 따라 홀수면 Dog, 짝수면 Bird를 생성하라 모든 동물의 move()를 호출하여 다형성이 구현됨을 확인하라

--- 소스.cpp
+++ 소스.cpp
@@ -1,10 +1,11 @@
-// 2023. 11. 27 월										(13주 1일)
+// 2023. 11. 29 수										(13주 2일)
 //---------------------------------------------------------------------------
 // class
 // 2023. 12. 11 월(15주 1일) - 한학기 정리, 책 소개
 // 2023. 12. 13 수(15주 2일) - 기말시험
 //---------------------------------------------------------------------------
 #include <iostream>
+#include <random>
 #include "save.h"
 #include "STRING.h"
 using namespace std;
@@ -16,15 +17,13 @@ using namespace std;
 // final을 붙이면 더 이상 자식 클래스를 만들어도 override 금지
 
 class Animal {
-	int age;
 public:
 	virtual void move() const {
 		cout << "동물 - 움직인다" << endl;
 	}
 };
 
-class Dog : public Animal{
-	bool bite;
+class Dog : public Animal {
 public:
 	virtual void move() const override {		// 조상의 맴버를 overriding 함
 		cout << "개 - 달린다" << endl;
@@ -32,30 +31,45 @@ public:
 };
 
 class Bird : public Animal {
-	long long fly;
 public:
 	virtual void move() const override final {
 		cout << "새 - 난다" << endl;
 	}
 };
 
+// [문제] 동물 10마리를 관리하는 코드를 만든다
+// 랜덤값에 따라 홀수면 Dog, 짝수면 Bird를 생성하라
+// 모든 동물의 move()를 호출하여 다형성이 구현됨을 확인하라
+
+default_random_engine dre;
+uniform_int_distribution uid{ 0, 1 };
+
 //--------
 int main()
 //--------
 {
-	// 다형성이라는 유연성을 구현하려면 메모리를 더 사용한다
-	// 핵심 키워드는 virtual
+	Animal* animals[10];
+	for (Animal*& p : animals) {
+		if (uid(dre)) {
+			p = new Dog;
+		}
+		else {
+			p = new Bird;
+		}
+	}
 
-	cout << "virtual 멤버가 있는 Dog의 크기 - " << sizeof(Dog) << endl;
+	for (int i = 0; i < 10; ++i) {
+		if (uid(dre)) {
+			animals[i] = new Dog;
+		}
+		else {
+			animals[i] = new Bird;
+		}
+	}
 
-	Dog d;
-	Bird b;
+	for (int i = 0; i < 10; ++i) {
+		animals[i]->move();
+	}
 
-	Animal* p = &d;
-	p->move();
-
-	p = &b;
-	p->move();
-
-	save("소스.cpp");
+	//save("소스.cpp");
 }
